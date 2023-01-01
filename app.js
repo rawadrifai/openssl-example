264c64fc5e00fe1e3cf29b3ec368ba217ee84781
Rawad Rifai <rawad.rifai@gmail.com>
2023-01-01 17:54:11 +0200

    initial and only commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const https = require("https");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+
+app.use("/", (req, res, next) => {
+  res.send("Hi world");
+});
+
+// app.use("/hello", (req, res, next) => {
+//   res.send("Hello world");
+// });
+
+const options = {
+  key: fs.readFileSync("cert/key.pem"),
+  cert: fs.readFileSync("cert/cert.pem"),
+};
+
+// https
+//   .createServer(options, (req, res) => {
+//     res.writeHead(200);
+//     res.end("hello world\n");
+//   })
+//   .listen(3000);
+// app.listen(3443, () => console.log("listening"));
+
+const sslServer = https.createServer(options, app);
+
+sslServer.listen(3000, () => console.log("Server started 🚀🚀🚀"));
